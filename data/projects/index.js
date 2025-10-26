6f8abb752232b0c90d54813fffbfca39a82d5554
n4n5 <its.just.n4n5@gmail.com>
2025-10-26 10:40:43 -0600

    mv files

--- data/projects/index.js
+++ data/projects/index.js
@@ -1,18 +1,15 @@
-import { readFile } from "fs/promises";
+import { readFile } from "node:fs/promises";
 
 async function extractLicenses() {
     try {
-        // Load and parse JSON
         const data = await readFile("projects.json", "utf8");
         const projects = JSON.parse(data);
 
-        // Extract licenseInfo field
         const dataExtracted = projects
             .map((project) => [project.name, project.diskUsage])
             .filter(([_name, disk]) => disk > 10000)
             .sort((a, b) => b[1] - a[1]);
 
-        // Print as JSON
         console.log(JSON.stringify(dataExtracted, null, 1));
     } catch (err) {
         console.error("Error:", err);
